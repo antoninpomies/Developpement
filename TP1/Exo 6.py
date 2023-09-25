57ee7899e60961b1d98bf2ad2b0532854740ed9f
antoninpomies@gmail.com <pomies@3il.fr>
2023-09-25 15:30:04 +0200

    Update Exo6.py

--- TP1/Exo 6.py
+++ TP1/Exo 6.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python3
 import sys
 import subprocess
-resultat = subprocess.run("curl ipinfo.io", shell=True,stdout=subprocess.PIPE)
-listinfo = resultat.stdout.splitlines()
-print(listinfo)
+# resultat = subprocess.run('ls *.py',shell=True,stdout=subprocess.PIPE)
+resultat = subprocess.run('curl ipinfo.io 2> /dev/null',shell=True,stdout=subprocess.PIPE)
+liste_fichiers = resultat.stdout.splitlines()
+print (liste_fichiers)
+
