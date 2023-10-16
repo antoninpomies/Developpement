36b873509068af869bec3ff7960897b2822819e7
antoninpomies@gmail.com <antoninpomies@gmail.com>
2023-10-16 09:18:24 +0200

    Ajout

--- TP1/Exo 9.py
+++ TP1/Exo 9.py
@@ -8,11 +8,14 @@ except Exception as e:
     print(e.args)
     sys.exit(1)
 
-nom = input("Saisissez Votre Nom : ")
-prenom = input("Saissisez Votre Prenom : ")
-adress = input("Saissisez Votre Addresse : ")
+nom = input("Nom : ")
+prenom = input("Prenom : ")
+adress = input("Addresse : ")
 
+#Methode par concaténation seulement avec des strings
 ligne = nom + ':' + prenom + ':' + adress + '\n'
+#Methode par substitution %s = chaine %d = entier
+ligne = '%s : %s : %s\n' % (nom, prenom, adress)
 
 descripteur.write(ligne)
 descripteur.close()
